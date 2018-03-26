67923b9afed434113ffdfbd499be45b72aa33b66
abiram98 <abiram98@gmail.com>
2018-03-26 12:27:06 +0200

    Destination.java

---
+++ src/Destination.java
@@ -0,0 +1,47 @@
+public class Destination {
+    private String destinationName;
+    private double lattitude;
+    private double longitude;
+    private String countryName;
+    private String cityType;
+
+    public String getDestinationName() {
+        return destinationName;
+    }
+
+    public void setDestinationName(String destinationName) {
+        this.destinationName = destinationName;
+    }
+
+    public double getLattitude() {
+        return lattitude;
+    }
+
+    public void setLattitude(double lattitude) {
+        this.lattitude = lattitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public String getCityType() {
+        return cityType;
+    }
+
+    public void setCityType(String cityType) {
+        this.cityType = cityType;
+    }
+}
