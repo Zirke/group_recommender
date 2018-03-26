3b1f957f2883095fddbda7882585769911c68977
TheKaergaard <31920988+TheKaergaard@users.noreply.github.com>
2018-03-26 17:54:49 +0200

    JavaFX update2

--- src/Main.java
+++ src/Main.java
@@ -8,9 +8,6 @@ import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
-import userProfiles.User;
-import java.util.ArrayList;
-import static userProfiles.User.*;
 
 public class Main extends Application {
 
@@ -27,41 +24,38 @@ public class Main extends Application {
 
         //Roots
         BorderPane first_menu_root = new BorderPane();
-        //first_menu_root.setLeft(add_leftside());
+
+        first_menu_root.getChildren().addAll(add_leftside());
+        first_menu_root.setLeft(add_leftside());
 
         //Scenes
         scene_main_menu = new Scene (first_menu_root);
-
-
-
         Main_menu.setMaximized(true);
+        Main_menu.setScene(scene_main_menu);
         Main_menu.setTitle("Big recommender system A400b (Liz)");
         Main_menu.show();
-
     }
 
-    public VBox add_leftside() {
+    private VBox add_leftside() {
         VBox vbox = new VBox();
         vbox.setPadding(new Insets(10));
         vbox.setSpacing(8);
 
-        Text title = new Text("Data");
-        title.setFont(Font.font("Calibri", FontWeight.BOLD, 14));
+        Text title = new Text("Test");
+        title.setFont(Font.font("Helvetica", FontWeight.BOLD, 14));
         vbox.getChildren().add(title);
 
         Label overview[] = new Label[]{
-                new Label("Sales"),
-                new Label("Marketing"),
-                new Label("Distribution"),
-                new Label("Costs")
+                new Label("Whatever"),
+                new Label("Whatever"),
+                new Label("Whatever"),
+                new Label("Whatever")
         };
 
         for (int i = 0; i < 4; i++) {
             VBox.setMargin(overview[i], new Insets(0, 0, 0, 8));
             vbox.getChildren().add(overview[i]);
         }
-        return add_leftside();
+        return vbox;
     }
-
-
 }
