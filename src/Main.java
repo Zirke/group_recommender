9dc22dc52628c5a3e1b239eb74695c7d5aa2b3ac
TheKaergaard <31920988+TheKaergaard@users.noreply.github.com>
2018-03-26 12:50:38 +0200

    JavaFX update

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import userProfiles.User;
 import java.util.ArrayList;
@@ -17,6 +24,43 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
         Main_menu = primaryStage;
+
+        //Roots
+        BorderPane first_menu_root = new BorderPane();
+        //first_menu_root.setLeft(add_leftside());
+
+        //Scenes
+        scene_main_menu = new Scene (first_menu_root);
+
+
+
+        Main_menu.setMaximized(true);
+        Main_menu.setTitle("Big recommender system A400b (Liz)");
+        Main_menu.show();
+
+    }
+
+    public VBox add_leftside() {
+        VBox vbox = new VBox();
+        vbox.setPadding(new Insets(10));
+        vbox.setSpacing(8);
+
+        Text title = new Text("Data");
+        title.setFont(Font.font("Calibri", FontWeight.BOLD, 14));
+        vbox.getChildren().add(title);
+
+        Label overview[] = new Label[]{
+                new Label("Sales"),
+                new Label("Marketing"),
+                new Label("Distribution"),
+                new Label("Costs")
+        };
+
+        for (int i = 0; i < 4; i++) {
+            VBox.setMargin(overview[i], new Insets(0, 0, 0, 8));
+            vbox.getChildren().add(overview[i]);
+        }
+        return add_leftside();
     }
 
 
