e775f04ef176f0714eef2f5fa712222ccea14094
TheKaergaard <31920988+TheKaergaard@users.noreply.github.com>
2018-03-26 12:14:57 +0200

    JavaFX start

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,20 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import userProfiles.User;
 import java.util.ArrayList;
 import static userProfiles.User.*;
 
-public class Main {
-    final String userProfileFile = "src/userProfiles/UserStoring.ser";
+public class Main extends Application {
+
+    Stage Main_menu;
+    Scene scene_main_menu;
 
     public static void main(String[] args) {
-
+        launch(args);
     }
+
+    
+
 
 }
