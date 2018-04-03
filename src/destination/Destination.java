7698ff6036d255177f0b5a892580a9fd15ba2785
abiram98 <abiram98@gmail.com>
2018-04-03 11:15:11 +0200

    destination package

---
+++ src/destination/Destination.java
@@ -0,0 +1,132 @@
+package destination;
+
+import java.io.*;
+import java.util.ArrayList;
+
+
+public class Destination {
+    private String destinationName;
+    private double lattitude;
+    private double longitude;
+    private String countryName;
+    private String cityType;
+    private ArrayList<Venue> venues = new ArrayList<Venue>();
+
+    public String getDestinationName() {
+        return destinationName;
+    }
+
+    public void setDestinationName(String destinationName) {
+        this.destinationName = destinationName;
+    }
+
+    public double getLattitude() {
+        return lattitude;
+    }
+
+    public void setLattitude(double lattitude) {
+        this.lattitude = lattitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public String getCityType() {
+        return cityType;
+    }
+
+    public void setCityType(String cityType) {
+        this.cityType = cityType;
+    }
+
+    public ArrayList<Venue> getVenues() {
+        return venues;
+    }
+
+    public void setVenues(ArrayList<Venue> venues) {
+        this.venues = venues;
+    }
+
+
+    public static ArrayList<Destination> listOfDestination() throws IOException,FileNotFoundException {
+        ArrayList<Destination> fileDestination = new ArrayList<Destination>();
+
+        FileReader fr = null;
+        String line;
+
+        //kan kun gøre det med abosulte path og ikke den relative
+        fr = new FileReader("/Users/Abiram/IdeaProjects/group_recommender/src/destination/cities.txt");
+        BufferedReader bf = new BufferedReader(fr);
+        int totalLine = linesInFile("/Users/Abiram/IdeaProjects/group_recommender/src/destination/cities.txt");
+
+
+
+        for(int i = 0; i<totalLine;i++) {
+            line = bf.readLine();
+            String[] strings = line.split("\\t",8);
+            Destination temp = new Destination();
+
+            if(isNumeric(strings[3]) && isNumeric(strings[4])){
+                temp.setDestinationName(strings[0] + strings[1] + strings[2] + strings[3]);
+                temp.setLattitude(Double.parseDouble(strings[4]));
+                temp.setLongitude(Double.parseDouble(strings[5]));
+                temp.setCountryName(strings[6]);
+                temp.setCityType(strings[7]);
+                fileDestination.add(temp);
+            }else if(isNumeric(strings[2]) && isNumeric(strings[3])){
+                temp.setDestinationName(strings[0] + strings[1] + strings[2]);
+                temp.setLattitude(Double.parseDouble(strings[3]));
+                temp.setLongitude(Double.parseDouble(strings[4]));
+                temp.setCountryName(strings[5]);
+                temp.setCityType(strings[6]);
+                fileDestination.add(temp);
+            }else{
+                temp.setDestinationName(strings[0]);
+                temp.setLongitude(Double.parseDouble(strings[1]));
+                temp.setLattitude(Double.parseDouble(strings[2]));
+                temp.setCountryName(strings[3]);
+                temp.setCityType(strings[5]);
+                fileDestination.add(temp);
+            }
+        }
+
+        try {
+            bf.close();
+            fr.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return fileDestination;
+    }
+
+    public static int linesInFile(String filePath) throws FileNotFoundException, IOException{
+        BufferedReader reader = new BufferedReader(new FileReader(filePath));
+        int lines = 0;
+        while (reader.readLine() != null) lines++;
+        reader.close();
+        return lines;
+    }
+    public static boolean isNumeric(String s) {
+        return s != null && s.matches("[-+]?\\d*\\.?\\d+");
+    }
+
+    public void printDestination(){
+        System.out.println("Destination name: " + this.getDestinationName() +
+                "   Lattitude :" + this.getLattitude() +"   Longitude :" + this.getLongitude() +
+                "   Country :" + this.getCountryName() + "   City type :" +this.getCityType());
+    }
+}
---
+++ src/destination/Venue.java
@@ -0,0 +1,111 @@
+package destination;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+
+import static destination.Destination.*;
+import static java.lang.Math.abs;
+
+//Used to read data from file
+public class Venue {
+    private String ID;
+    private double Latitude;
+    private double longitude;
+    private String CategoryName;
+    private String CountryCode;
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public double getLatitude() {
+        return Latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        Latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getCategoryName() {
+        return CategoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        CategoryName = categoryName;
+    }
+
+    public String getCountryCode() {
+        return CountryCode;
+    }
+
+    public void setCountryCode(String countryCode) {
+        CountryCode = countryCode;
+    }
+
+    public static ArrayList<Venue> listOfVenues() throws IOException{
+        ArrayList<Venue> listOfVenues = new ArrayList<>();
+        Venue temp = new Venue();
+        final String filePath = "/Users/Abiram/IdeaProjects/group_recommender/src/data/dataset_TIST2015_POIs.txt";
+        FileReader fr = new FileReader(filePath);
+        BufferedReader bf = new BufferedReader(fr);
+        String line;
+        int totalLine = linesInFile(filePath);
+
+        for(int i = 0; i< totalLine;i++){
+            line = bf.readLine();
+            String[] strings = line.split("\\t",5);
+            temp.setID(strings[0]);
+            temp.setLatitude(Double.parseDouble(strings[1]));
+            temp.setLongitude(Double.parseDouble(strings[2]));
+            temp.setCategoryName(strings[3]);
+            temp.setCountryCode(strings[4]);
+            listOfVenues.add(temp);
+        }
+        return listOfVenues;
+    }
+
+    //vi skal måske samle POI sammen på en anden måde.
+    public static void destinationVenues() throws IOException {
+        try {
+            ArrayList<Venue> listOfVenues = listOfVenues();
+            ArrayList<Destination> listOfDestination = listOfDestination();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        int totalDestination = 415;
+        int totalVenue = linesInFile("/Users/Abiram/IdeaProjects/group_recommender/src/data/dataset_TIST2015_POIs.txt");
+        int destIndex = 0;
+        double distVenueDest;
+        double indexDist;
+        for(int i = 0; i< totalVenue;i++){
+            for(int j = 1; j< totalDestination; j++){
+
+                distVenueDest = abs(listOfDestination().get(j).getLattitude()-listOfVenues().get(i).getLatitude()) +
+                        abs(listOfDestination().get(j).getLongitude()-listOfVenues().get(i).getLongitude());
+                indexDist = abs(listOfDestination().get(destIndex).getLattitude()-listOfVenues().get(i).getLatitude()) +
+                        abs(listOfDestination().get(destIndex).getLongitude()-listOfVenues().get(i).getLongitude());
+                //System.out.println("h");
+                if(distVenueDest < indexDist){
+                    destIndex = j;
+                }
+            }
+            listOfDestination().get(destIndex).getVenues().add(listOfVenues().get(i));
+        }
+
+    }
+}
