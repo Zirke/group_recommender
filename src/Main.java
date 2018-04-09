4a6ec1dcda9411a0db5c3703bb61137a89fb46fd
abiram98 <abiram98@gmail.com>
2018-04-09 22:49:06 +0200

    user interface package

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-import destination.Destination;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Rectangle2D;
@@ -7,35 +6,33 @@ import javafx.scene.Scene;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
-import static destination.Venue.destinationVenues;
+import static javafx.application.Application.launch;
 
 /*
   Main Application. This class handles navigation and user interface.
 */
 
-public class Main{
+public class Main extends Application{
 
     public static void main(String[] args) {
-        try {
+        /*try {
             destinationVenues();
         } catch (IOException e) {
             e.printStackTrace();
-        }
-        //launch(args);
+        }*/
+        launch(args);
     }
 
-    /*@Override
+    @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FrontPage.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("userInterface/FrontPage.fxml"));
         Rectangle2D visualBounds = Screen.getPrimary().getVisualBounds();
 
         Scene scene = new Scene(root, visualBounds.getWidth(), visualBounds.getHeight());
 
         primaryStage.setScene(scene);
-       // primaryStage.setMaximized(true); //sets size of stage to windowed fullscreen
+       primaryStage.setMaximized(true); //sets size of stage to windowed fullscreen
         primaryStage.setTitle("Test");
         primaryStage.show();
-    }*/
+    }
 }
