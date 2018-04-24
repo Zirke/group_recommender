018e5d2e2b63afb96de5aaa9bdab1ba8d6cd0953
TheKaergaard <31920988+TheKaergaard@users.noreply.github.com>
2018-04-24 16:21:20 +0200

    group.java

---
+++ src/userProfiles/Group.java
@@ -0,0 +1,26 @@
+package userProfiles;
+
+import java.util.ArrayList;
+
+public class Group {
+    private String groupID;
+    private ArrayList<User> usersInGroup;
+
+    public void setGroupID(String groupID) {
+        this.groupID = groupID;
+    }
+
+    public void setUsersInGroup(ArrayList<User> usersInGroup) {
+        this.usersInGroup = usersInGroup;
+    }
+
+    public String getGroupID() {
+        return groupID;
+    }
+
+    public ArrayList<User> getUsersInGroup() {
+        return usersInGroup;
+    }
+
+
+}
