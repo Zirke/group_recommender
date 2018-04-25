c178f005ea3e5dc4dc18108ef323f874a4412973
lisaniemeyer <d@d.d>
2018-04-25 18:16:51 +0200

    ghv

--- src/UserInterface/ProfilePageController.java
+++ src/UserInterface/ProfilePageController.java
@@ -3,21 +3,29 @@ package UserInterface;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import org.controlsfx.control.textfield.TextFields;
 import userProfiles.User;
 
 import java.io.IOException;
+import java.lang.reflect.Field;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class ProfilePageController extends GeneralController {
+public class ProfilePageController extends GeneralController implements Initializable {
     @FXML
     private Button FrontPageButton, showRecommendationsButton, managetravelGroupsButton;
     @FXML
     private Label usernameLabel, firstnameLabel, lastnameLabel, genderLabel, ageLabel;
+    @FXML
+    private TextField searchField;
 
     public void initializeLoggedInUserData (User user) {
         usernameLabel.setText(user.getUsernameID());
@@ -35,6 +43,13 @@ public class ProfilePageController extends GeneralController {
     @FXML
     public void pressManageTravelGroups(ActionEvent event) {
         LoadUI("GroupPage", event);
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb){
+        String[] options = {"hej", "hello", "fuck"};
+
+        TextFields.bindAutoCompletion(searchField, options);
     }
 
     @FXML
